0bc277465dfb49482af64f38521461471af450d0
SickMyDucks <me@neilrichter.com>
2018-06-08 10:24:33 +0200

    Added multiple contraints requirements with regex on a single string

---
+++ regex-patterns-validation.js
@@ -0,0 +1,45 @@
+const options = {
+    length: {
+        minChar: 4,
+        maxChar: 16
+    },
+    digitNumber: 1,
+    specCharNumber: 1,
+    majNumber: 1
+};
+
+const patterns = {
+    checkLength: options.length.maxChar == null ? new RegExp(`.{${options.length.minChar},}`) : new RegExp(`.{${options.length.minChar},${options.length.maxChar}}`),
+    digitNumber: new RegExp(`(?=(.*[0-9]){${options.digitNumber},})`),
+    checkSpecialCharacter: new RegExp(`(?=(.*[$@$!%*#?&_{|}+.']){${options.specCharNumber},})`),
+    checkCapitalCharacter: new RegExp(`(?=(.*[A-Z]){${options.majNumber},})`)
+};
+
+const validations = {};
+var strings = ['Str1ng to {test}', 'string to test'];
+
+for (let i in patterns) {
+    validations[i] = patterns[i].test(strings[0]);
+}
+console.log(validations);
+/**
+ * {
+ *     checkLength: true,
+ *     digitNumber: true,
+ *     checkSpecialCharacter: true,
+ *     checkCapitalCharacter: true
+ * }
+ */
+
+for (let i in patterns) {
+    validations[i] = patterns[i].test(strings[1]);
+}
+console.log(validations);
+/**
+ * {
+ *     checkLength: true,
+ *     digitNumber: false,
+ *     checkSpecialCharacter: false,
+ *     checkCapitalCharacter: false
+ * }
+ */
