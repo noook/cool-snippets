32a3453ea0b2b13444df11d8ad53a9e1da3464e3
neilrichter <me@neilrichter.com>
2018-07-05 16:21:00 +0200

    Added iterating through a collection of objects with possible undefined key

---
+++ undefined-key.js
@@ -0,0 +1,34 @@
+/**
+ * Iterating in a collection of objects that may not contain
+ * a key, and prevent the script from crashing
+ */
+
+const collection = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 42,
+    options: {
+      height: 180
+    }
+  },
+  {
+    id: 2,
+    name: "Mary Jane",
+    age: 26,
+  },
+  {
+    id: 3,
+    name: "Sam Tok",
+    age: 42,
+    options: {
+      height: 176
+    }
+  },
+];
+
+for (let i in collection) {
+  // Causes a crash with error : Can't read property 'height' of undefined
+  // console.log(collection[i].options.height);
+  console.log((collection[i].options || {}).height || "Not defined");
+}
